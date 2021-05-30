747d41c3c21c8d8d429cc7a42923236a24b8c1a6
gabrielbf <gabrielbp+github@gmail.com>
2021-05-29 23:39:06 -0300

    Primeiro commit

---
+++ workspace/Atividade1/src/br/com/gabrielfritzen/Exercicio2.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package br.com.gabrielfritzen;
+
+/**
+ * @author Gabriel
+ *
+ */
+public class Exercicio2 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
